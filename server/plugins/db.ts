d4f323e6c24bcb287d003689a1b3d3c22bb9f750
ImDarkTom <darktomg21@gmail.com>
2025-12-13 14:35:18 +0000

    fix: models not being added in production

--- server/plugins/db.ts
+++ server/plugins/db.ts
@@ -1,10 +1,19 @@
 import { connectDb } from '~~/lib/db/db';
 import mongoose from 'mongoose';
+import { User } from '../models/user';
+import { Review } from '../models/review';
 
 export default defineNitroPlugin(async (nitroApp) => {
     try {
         await connectDb()
         console.log("Connected to MongoDB.");
+
+        // Issue related to tree-shaking removing the models.
+        // https://github.com/nuxt/nuxt/issues/26531#issuecomment-2025118655
+        const _ = [
+            User.length,
+            Review.length
+        ];
     } catch (e) {
         console.error(`Error connecting to MongoDB: ` + e);
     }
